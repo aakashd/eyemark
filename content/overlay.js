fd5de719b8f933c7c8f81c8a7b3ea173776f79d7
aakash dharmadhikari <aakashd@gmail.com>
2010-03-04 21:44:45 +0530

    created project skeleton using extensionwiz

---
+++ content/overlay.js
@@ -0,0 +1,27 @@
+var eyemark = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("eyemark-strings");
+    document.getElementById("contentAreaContextMenu")
+            .addEventListener("popupshowing", function(e) { this.showContextMenu(e); }, false);
+  },
+
+  showContextMenu: function(event) {
+    // show or hide the menuitem based on what the context menu is on
+    // see http://kb.mozillazine.org/Adding_items_to_menus
+    document.getElementById("context-eyemark").hidden = gContextMenu.onImage;
+  },
+  onMenuItemCommand: function(e) {
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(Components.interfaces.nsIPromptService);
+    promptService.alert(window, this.strings.getString("helloMessageTitle"),
+                                this.strings.getString("helloMessage"));
+  },
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    eyemark.onMenuItemCommand(e);
+  }
+
+};
+window.addEventListener("load", function(e) { eyemark.onLoad(e); }, false);
---
+++ defaults/preferences/eyemark.js
@@ -0,0 +1,5 @@
+pref("extensions.eyemark.boolpref", false);
+pref("extensions.eyemark.intpref", 0);
+pref("extensions.eyemark.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.eyemark@aakash.dharmadhikari.description", "chrome://eyemark/locale/eyemark.properties");
